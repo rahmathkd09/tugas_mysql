fc109e0b4421782816453b9c73fe32111d47cf2c
rahmathkd09 <rahmathkd09@gmail.com>
2020-10-14 13:01:00 +0700

    Update tugas3film_enum.sql

--- tugas3film_enum.sql
+++ tugas3film_enum.sql
@@ -1,11 +1,11 @@
 CREATE TABLE film(NO TINYINT,judul ENUM('Avatar','Titanic',
-'Star Wars: The Force Awakens','Jurassic World','The Avenger'),
+'Star Wars: The Force Awakens','Jurassic World','The Avengers'),
 pendapatan BIGINT,tahun_produksi YEAR(4));
 
 INSERT INTO film VALUES
 (1,'Avatar',2787965087,2009),
 (2,'Titanic',2186772302,1997),
 (3,'Star Wars: The Force Awakens',2068223624,2015),
-(4,'Jurassic World',16717132018,2015),
-(5,'The Avenger',1518812988,2012);
+(4,'Jurassic World',1671713208,2015),
+(5,'The Avengers',1518812988,2012);
 SELECT * FROM film;
